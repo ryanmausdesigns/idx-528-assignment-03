dadc64b3ab016b23130506343399d4411bc77ce8
Ryan Maus <rmaus@id.iit.edu>
2018-09-13 15:35:30 -0500

    adding assignment-03
    includes .html and .js file

---
+++ script.js
@@ -0,0 +1,39 @@
+var name = "Ryan";
+var age = 29; 
+var age = [
+    26,
+    27,
+    28,
+    29,
+    30,
+];
+console.log ( age );
+var last_name = "Maus"; 
+console.log (name + " " + last_name)
+var fruits = [ 
+
+];
+var fruits = ["Apples", "Oranges", "Pears", "Bananas",  "Cherries"]
+console.log ( fruits )
+console.log(fruits.length)
+var first = fruits[0]; 
+console.log( first)
+var last = fruits[fruits.length - 1]; 
+console.log( last )
+
+let animal_a = {
+    type: "cat",
+    age: 6,
+    name: "Fry",
+    speak: "Meow!",
+};
+let animal_b = {
+    type: "dog",
+    age: 3,
+    name: "Mabel",
+    speak: "woof!",
+};
+animal_b.is_outdoor = "true";
+console.log(animal_b.name + " is " + animal_b.age + " years old")
+
+
